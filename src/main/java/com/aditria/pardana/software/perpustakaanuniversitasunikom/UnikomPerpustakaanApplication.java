b0418c5d4592bcf9f050703750bc3a44f3ff466c
pardana <aditria.pardana20@gmail.com>
2017-09-07 11:40:16 +0700

    membuat project

---
+++ src/main/java/com/aditria/pardana/software/perpustakaanuniversitasunikom/UnikomPerpustakaanApplication.java
@@ -0,0 +1,12 @@
+package com.aditria.pardana.software.perpustakaanuniversitasunikom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikomPerpustakaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnikomPerpustakaanApplication.class, args);
+	}
+}
